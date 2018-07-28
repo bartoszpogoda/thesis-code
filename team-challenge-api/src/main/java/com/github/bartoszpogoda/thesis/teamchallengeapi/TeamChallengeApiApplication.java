75031381427a09ba103aa9bbdb93f90318bf7f58
bartoszpogoda <bartek.pogoda1@gmail.com>
2018-07-28 11:47:26 +0200

    project init, base structure and generated code

---
+++ team-challenge-api/src/main/java/com/github/bartoszpogoda/thesis/teamchallengeapi/TeamChallengeApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.bartoszpogoda.thesis.teamchallengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamChallengeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamChallengeApiApplication.class, args);
+    }
+}
